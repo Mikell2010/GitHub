3d3d4ecd381b6e47ce43177625c78cb18e979897
Michel <mschocken@gmail.com>
2023-05-04 20:50:34 -0400

    arreglo

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 
@@ -19,26 +18,4 @@ for (let i = 1; i <= 20; i++) {
       console.log(i);
     }
   }
-=======
-
-
-
-for (var i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
-}
-
-for (let i = 1; i <= 20; i++) {
-    if (i % 2 !== 0) {
-      console.log(i);
-    }
-  }
->>>>>>> b0976587a9eac4976f93910f0e818719dca03642
   
